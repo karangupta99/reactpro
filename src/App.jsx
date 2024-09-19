c021d3391f82feedc2c280b266952a48b77a6818
karangupta99 <jrkaran85@gmail.com>
2024-09-20 00:54:49 +0530

    secret

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ const App = () => {
       <h2 className='text-green-800 font-bold text-2xl'>hello friend </h2>
       <h1 className='text-blue-800 font-bold text-2xl'>react is working now</h1>
       <p className='text-red-800 font-bold text-2xl'>thanku</p>
+      <h4>{import.meta.env.VITE_SECRET_KEY}</h4>
       </div>
      
 
