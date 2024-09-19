817759e2956bbe00047a46fa42fa1a10698deda7
karangupta99 <jrkaran85@gmail.com>
2024-09-20 00:49:10 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <div className=' px-3 py-24 flex items-center gap-5 justify-center'>
+      <h2 className='text-green-800 font-bold text-2xl'>hello friend </h2>
+      <h1 className='text-blue-800 font-bold text-2xl'>react is working now</h1>
+      <p className='text-red-800 font-bold text-2xl'>thanku</p>
+      </div>
+     
+
+
+    </div>
+  )
+}
+
+export default App
